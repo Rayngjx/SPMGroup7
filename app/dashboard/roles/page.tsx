0a131271f276a8f5baeafa40b5edc012710755f4
jontoh <75199733+jontohh@users.noreply.github.com>
2024-10-06 15:40:44 +0800

    app: CRUD roles; app: api: config routes for CRUD; prisma: switched to supabase db
    prisma: if want to run these changes, use supabase .env config

--- app/dashboard/roles/page.tsx
+++ app/dashboard/roles/page.tsx
@@ -1,39 +1,41 @@
 import { auth } from '@/auth';
 import { Breadcrumbs } from '@/components/breadcrumbs';
+import Component_Roles from '@/components/dashboard/roles/RolesPannel';
 import PageContainer from '@/components/layout/page-container';
-import { UserClient } from '@/components/tables/staff-tables/client';
-import { db } from '@/lib/db';
 import React from 'react';
-import { cache } from 'react';
 
 const breadcrumbItems = [
   { title: 'Dashboard', link: '/dashboard' },
-  { title: 'Staff', link: '/dashboard/staff' }
+  { title: 'Roles', link: '/dashboard/roles' }
 ];
 
 export default async function page() {
-  let getUsers;
-  try {
-    let data = await fetch('http://localhost:3000/api/users/all');
-    getUsers = await data.json();
-  } catch (error) {
-    console.error(error);
-  }
+  //   let getUsers;
+  //   try {
+  //     let data = await fetch('http://localhost:3000/api/users/all')
+  //     getUsers = await data.json()
+  //   } catch (error) {
+  //     console.error(error);
+  //   }
 
   const session = await auth();
 
-  getUsers = getUsers ?? []; // Provide a default value if getUsers is undefined
+  const data = await fetch('http://localhost:3000/api/roles/all');
+  const roles = await data.json();
+
+  //   getUsers = getUsers ?? []; // Provide a default value if getUsers is undefined
 
   return (
     <PageContainer>
       {
         // This is a conditional rendering of the page title
-        session?.user?.role_id === 3 ? (
+        session?.user?.role_id === 2 ? (
           <>
-            <h1 className="text-3xl font-semibold">View Staff Dashboard</h1>
+            <h1 className="text-3xl font-semibold">Roles</h1>
             <div className="space-y-2">
               <Breadcrumbs items={breadcrumbItems} />
-              <UserClient data={getUsers} />
+              <Component_Roles dataRoles={roles} />
+              {/* <UserClient data={getUsers} /> */}
             </div>
           </>
         ) : (
