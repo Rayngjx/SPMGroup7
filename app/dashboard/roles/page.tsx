ce9796fc022fb81f202eb375e4946d4bfe4e3732
jontoh <75199733+jontohh@users.noreply.github.com>
2024-10-20 13:58:37 +0800

    src: roles + users + added helper methods: to check for correct type of role

--- app/dashboard/roles/page.tsx
+++ app/dashboard/roles/page.tsx
@@ -1,7 +1,12 @@
+import {
+  checkIfSeniorManagementOrHR,
+  checkIfStaff
+} from '@/app/helper/userService';
 import { auth } from '@/auth';
 import { Breadcrumbs } from '@/components/breadcrumbs';
 import Component_Roles from '@/components/dashboard/roles/RolesPannel';
 import PageContainer from '@/components/layout/page-container';
+import { is } from 'date-fns/locale';
 import React from 'react';
 
 const breadcrumbItems = [
@@ -23,13 +28,19 @@ export default async function page() {
   const data = await fetch('http://localhost:3000/api/roles');
   const roles = await data.json();
 
+  let isSeniorOrHR = false;
+
+  if (session) {
+    isSeniorOrHR = await checkIfSeniorManagementOrHR(session.user.staff_id);
+  }
+
   //   getUsers = getUsers ?? []; // Provide a default value if getUsers is undefined
 
   return (
     <PageContainer>
       {
         // This is a conditional rendering of the page title
-        session?.user?.role_id === 1 ? (
+        isSeniorOrHR ? (
           <>
             <h1 className="text-3xl font-semibold">Roles</h1>
             <div className="space-y-2">
@@ -41,7 +52,7 @@ export default async function page() {
         ) : (
           <>
             <h1 className="text-3xl font-semibold">Staff Dashboard</h1>
-            <p>Access Denied</p>
+            <p>Access Denied. Ask HR or sr mgr</p>
           </>
         )
       }
---
+++ app/helper/userService.ts
@@ -0,0 +1,20 @@
+import { users } from '@prisma/client';
+
+// userService.js
+export async function getUser(userId: users['staff_id']) {
+  const response = await fetch(
+    `http://localhost:3000/api/users?staff_id=${userId}`
+  );
+  const user = await response.json();
+  return user;
+}
+
+export async function checkIfSeniorManagementOrHR(userId: users['staff_id']) {
+  const user = await getUser(userId);
+  return user.role_id === 1 || user.department === 'HR';
+}
+
+export async function checkIfStaff(userId: users['staff_id']) {
+  const user = await getUser(userId);
+  return user.role_id === 2;
+}
